a2db79ba4641bc2a33c4b26dfb71fbbc54ec9959
Honsy0105 <121519816+Honsy0105@users.noreply.github.com>
2023-11-11 13:43:30 +0100

    Delete getElement.js

--- getElement.js
+++
@@ -1,9 +0,0 @@
-export const getElement = (selection) => {
-  const element = document.querySelector(selection)
-  if (element) {
-    return element
-  } 
-  throw new Error(
-    `Please check ${selection} selector, There is an Error`
-  )
-}
